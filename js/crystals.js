96eee29f9ca2dabd4de3c2ff1653754cc8a81c83
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-08-26 17:34:10 -0700

    update js for game

---
+++ js/crystals.js
@@ -0,0 +1,32 @@
+var numberToGuess = Math.floor(Math.random()*20);
+var userCount = 0;
+var crystalOptions = [10, 3, 5, 7];
+
+$("#number-to-guess").text(function(){
+	"Number to Guess" + numberToGuess;
+console.log(numberToGuess)
+});
+
+for (var i = 0; i < crystalOptions.length; i++) {
+	$("#crystal").attr("data-crystalvalue", crystalOptions[i]);
+};
+
+$(".crystal").on("click", function(){
+	var crystalValue = $(this).attr("data-crystalvalue");
+	crystalValue = parseInt(crystalValue);
+	userCount += crystalValue;
+
+	alert("New Score: " + userCount);
+
+	if (userCount === numberToGuess){
+		alert("you won!");
+	}
+
+	else if (userCount >= numberToGuess){
+		confirm("Oh no! You lost! Try again");
+		if (confirm === true){
+			//reset game
+		}
+	};
+	
+});
