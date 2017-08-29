11ef5a9115d66bda5e3a3a83e9d43acafb911942
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-08-28 21:08:48 -0700

    update js

--- js/crystals.js
+++ js/crystals.js
@@ -1,38 +1,37 @@
 var numberToGuess = 0;
-var userCount = 0;
-var crystalOptions = [10, 3, 5, 7];
+
 
 $(document).ready(function(){
 
-numberToGuess = Math.floor(Math.random()*20);
+numberToGuess = Math.floor(Math.random()*88)+19;
 
 $("#number-to-guess").text(function(){
-	"Number to Guess" + numberToGuess;
-	console.log(numberToGuess)
+	return "Number to Guess: " + numberToGuess;
 });
 
-for (var i = 0; i < crystalOptions.length; i++) {
-	$("#crystal").attr("data-crystalvalue", crystalOptions[i]);
-};
+$("#crystal-blue").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
+$("#crystal-red").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
+$("#crystal-green").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
+$("#crystal-pink").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
 
 $(".crystal").on("click", function(){
 	var crystalValue = $(this).attr("data-crystalvalue");
 	crystalValue = parseInt(crystalValue);
 	userCount += crystalValue;
 
-	console.log(crystalOptions);
-
 	alert("New Score: " + userCount);
 
 	if (userCount === numberToGuess){
 		alert("you won!");
+		//score ++
 	}
 
 	else if (userCount >= numberToGuess){
 		confirm("Oh no! You lost! Try again");
 		if (confirm === true){
 			//reset game
-		}
+			//loss ++
+		};
 	};
 	
 });
