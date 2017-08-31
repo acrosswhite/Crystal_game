79b4a8347329b78226dcc05482a662bc22e074a0
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-08-30 20:40:20 -0700

    game working

--- js/crystals.js
+++ js/crystals.js
@@ -3,50 +3,65 @@ var userCount = 0;
 var winScore = 0;
 var lossScore = 0;
 
-
-$(document).ready(function(){
+function resetGame (){
+	numberToGuess = 0;
+	userCount = 0;
 
 	$("#crystal-blue").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
 	$("#crystal-red").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
 	$("#crystal-green").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
 	$("#crystal-pink").attr("data-crystalvalue", Math.floor(Math.random()*12)+1);
 
+
+	numberToGuess = Math.floor(Math.random()*101)+19;
+
+	$("#number-to-guess").text(function(){
+		return "Number to Guess: " + numberToGuess;
+	});
+};
+
+
+function playGame () {
+
 	$(".crystal").on("click", function(){
 		var crystalValue = $(this).attr("data-crystalvalue");
 		crystalValue = parseInt(crystalValue);
 		userCount += crystalValue;
-
 		alert("New Score: " + userCount);
-	});
-
-	function runGame (){
-		numberToGuess = Math.floor(Math.random()*101)+19;
-
-		$("#number-to-guess").text(function(){
-			return "Number to Guess: " + numberToGuess;
-		});
 
 		if (userCount === numberToGuess){
 			alert("you won!");
 			winScore ++;
-			$("#wins").text("Wins:" + winScore);
-			numberToGuess = 0;
-			userCount = 0;
-			runGame ();
+			$("#wins").html("Wins:" + winScore);
+			resetGame ();
 		};
 
-		if (userCount >= numberToGuess){
-			confirm("Oh no! You lost! Try again");
+		if (userCount > numberToGuess){
+			console.log("you lose");
+			var playAgain = confirm("Oh no! You lost! Try again");
 			lossScore ++;
-			$("#losses").text("Losses: " + lossScore);
-			if (confirm === true){
-				numberToGuess = 0;
-				userCount = 0;
-				runGame ();
+			$("#losses").html("Losses: " + lossScore);
+			if (playAgain === true){
+				resetGame ();
 			};
 		};
+
 	});
+
+};
+
+
+$(document).ready(function(){
+
+	function runGame (){
+
+	resetGame ();
+	playGame ();
+
+	};
+
 runGame ();
+
 });
 
 //create a reset instead of calling runGame inside the runGame function
